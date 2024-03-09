e1eabc4a117b0cff72a95ef65bb122aba032d483
mohammadmurtada <moha135h@stud.kea.dk>
2024-03-10 00:18:13 +0100

    Lavede de forskellige components til de forskellige sider der skal laves

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './core/auth/login/login.component';
+import { HomepageComponent } from './features/homepage/homepage.component';
+import { ProductsComponent } from './features/products/products.component';
+import { AboutUsComponent } from './features/about-us/about-us.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { NavigationbarComponent } from './@shared/navigationbar/navigationbar.component';
+import { BasketComponent } from './features/basket/basket.component';
+import { FavouritesComponent } from './features/favourites/favourites.component';
+import { FooterComponent } from './@shared/footer/footer.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
+import { ForgotPasswordComponent } from './core/auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './core/auth/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomepageComponent,
+    ProductsComponent,
+    AboutUsComponent,
+    ContactComponent,
+    NavigationbarComponent,
+    BasketComponent,
+    FavouritesComponent,
+    FooterComponent,
+    CheckoutComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
