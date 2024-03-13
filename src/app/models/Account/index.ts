8073d197266da7d7ccd02baf422727e3201ccaf3
mohammadmurtada <moha135h@stud.kea.dk>
2024-03-13 10:54:06 +0100

    Det næste at lave er produkt overview

---
+++ src/app/models/Account/index.ts
@@ -0,0 +1,11 @@
+export interface Account {
+    id: string;
+    firstName: String;
+    lastName: String;
+    address: String;
+    city: String;
+    postalCode: Number;
+    email: String;
+    phone: String;
+    password: String;
+}
